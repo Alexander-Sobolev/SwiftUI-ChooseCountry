0028e7245c3b336f4625acece778fd179caae6f3
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-06 10:03:58 +0300

    Initial Commit

---
+++ SwiftUIChooseCountry/SwiftUIChooseCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChooseCountryApp.swift
+//  SwiftUIChooseCountry
+//
+//  Created by Alexander Sobolev on 06.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChooseCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
